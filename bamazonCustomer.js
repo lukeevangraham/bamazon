2d05f432f90b68db7bfcd3a5f6d27625787f7d66
Cefalu Wave <luke.evan.graham@gmail.com>
2019-05-14 23:35:14 -0700

    working with first prompts

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -17,11 +18,11 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    afterConnection();
+    displayTable();
 });
 
 
-function afterConnection() {
+function displayTable() {
     connection.query("SELECT * FROM bamazon.products", function (err, res) {
         if (err) throw err;
 
@@ -31,22 +32,38 @@ function afterConnection() {
         //  INCLUDING ID'S NAMES AND PRICES
         for (let i = 0; i < res.length; i++) {
             console.log("ID: " + res[i].item_id + " ---- Product: " + res[i].product_name + " ---- Price: $" + res[i].price)
-            
         }
+        runSearch();
+    });
+}
 
-        connection.end();
+//  THEN PROMPT USER WITH TWO MESSAGES
+
+function runSearch() {
+    //  ASK USER THE ID OF THE PRODUCT THEY WOULD LIKE TO BUY
+    inquirer
+    .prompt({
+        name: "productDesired",
+        type: "input",
+        message: "Enter ID of product you want to buy: "
     })
-        ;
+    .then(function(answer) {
+        //  ASK USER HOW MANY UNITS OF THE PRODUCT THEY WOULD LIKE TO BUY
+        inquirer
+        .prompt({
+            name: "quantityDesired",
+            type: "input",
+            message: "How many units of the product would you like to buy? "
+        })
+        .then(console.log("Great!\n\n"));
+    })
+    connection.end();
 }
 
 
 
 
-//  THEN PROMPT USER WITH TWO MESSAGES
 
-//  ASK USER THE ID OF THE PRODUCT THEY WOULD LIKE TO BUY
-
-//  ASK USER HOW MANY UNITS OF THE PRODUCT THEY WOULD LIKE TO BUY
 
 //  CHECK THAT STORE HAS ENOUGH PRODUCE FOR USER REQUEST
 
@@ -54,7 +71,7 @@ function afterConnection() {
 
 //  IF PRODUCT IS AVAILABLE FULFILL ORDER
 
-    // UPDATE SQL DATABASE TO REFLECT REMAINING QUANTITY
+// UPDATE SQL DATABASE TO REFLECT REMAINING QUANTITY
 
-    // SHOW USER TOTAL PURCHASE COST
+// SHOW USER TOTAL PURCHASE COST
 
